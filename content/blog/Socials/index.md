12f547a3d874640642c79b0df4dbc708bb91e6a7
Yuriy <>
2020-12-08 23:59:51 +0800

    inital commit (header done)

--- content/blog/Socials/index.md
+++ content/blog/Socials/index.md
@@ -1,5 +1,5 @@
 ---
-title: My Second Post!
+title: Socials
 date: "2015-05-06T23:46:37.121Z"
 ---
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,52 @@
 import React from "react"
-import { Link } from "gatsby"
+import phoneLogo from '../img/logo.png'
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  const header = (
+    <nav
+      className="navbar is-transparent"
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div
+        style={{
+          height: 125,
+          background: '#8C5ACC',
+          borderBottomRightRadius: 35,
+          borderBottomLeftRadius: 35,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}
+      >
+        <img
+          src={phoneLogo}
+          alt="phone-logo"
+          style={{
+          height: 65,
+          borderRadius: 1000,
+          width: 'auto',
+          background:'#ffffff',
+          marginLeft: 12
+          }}
+        />
+        <p
+          style={{
+          fontSize: 32,
+          letterSpacing: 1.25,
+          fontFamily: 'Montserrat',
+          fontWeight: 600,
+          margin: 20,
+          color: '#ffffff'
+          }}
+        >
+          onecard.bio
+        </p>
+      </div>
+    </nav>
+  )
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
